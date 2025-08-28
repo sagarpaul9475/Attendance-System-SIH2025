6a2223038bf069bc15b599de6447b11aa7d4e3dd
Devraj39 <devrajsingh7505973410@gmail.com>
2025-08-28 22:24:37 +0530

    Added Student and teacher model

---
+++ Backend/models/studentLogin.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+const studentLoginSchema = new mongoose.Schema({
+  studentId: {
+    type: Number,
+    required: true,
+    unique: true
+  },
+  name: {
+    type: String,
+    required: true
+  },
+  gmail: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  }
+}, { timestamps: true });
+
+module.exports = mongoose.model('StudentLogin', studentLoginSchema);
+
+
